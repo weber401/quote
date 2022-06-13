3644592d37c58845e8849f0cb0ea75f331db8891
weber401 <dipendramahat581@gmail.com>
2022-06-13 16:36:53 +0545

    setup git

--- javascript.js
+++ javascript.js
@@ -1,37 +1,73 @@
-
+//TODO
+//THE RECIVED JSON HAS NOT TITLE, MAKE IT RETRIVE ONE JSON QUOTE AT A TIME
 
 const projectName = "random quote machine"
 
-const currentQuote = ""
-const currentAuthor = ""
-let quotesData; 
+var currentQuote = ''
+var currentAuthor = ''
+let quotesData;  
 
 // for api call
-const settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://type.fit/api/quotes",
-    "method": "GET"
-  }
-  
-  $.ajax(settings).done(function (response) {
-    const data = JSON.parse(response);
-    console.log(data);
-    quotesData = data;
-  });
+function getQuotes(){
+// const settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "https://type.fit/api/quotes",
+//   "method": "GET"
+// }
 
-//for selecting random quote
+// $.ajax(settings).done(function (response) {
+//   const data = JSON.parse(response);
+//   quotesData = JSON.parse(response)
+//   console.log("Data:")
+//   console.log(quotesData);
+
+  return $.ajax({
+    headers: {
+      Accept: 'application/json'
+    },
+    url:
+      'https://type.fit/api/quotes',
+    success: function (jsonQuotes) {
+      if (typeof jsonQuotes === 'string') {
+        quotesData = JSON.parse(jsonQuotes);
+        console.log('quotesData');
+        console.log(quotesData);
+      }
+    } 
+});
+}
+ 
 function getRandomQuote() {
-    return quotesData.quotes[
-      Math.floor(Math.random() * quotesData.quotes.length)
-    ];
-  }
-  
-  function getQuote() {
-    let randomQuote = getRandomQuote();
-  
-    currentQuote = randomQuote.quote;
+  // for (var number in quotesData){
+  //   if (number ==Math.floor(Math.random() * 100)){
+  //     console.log(number)
+  //     console.log(quotesData[number])
+  //     return quotesData[number]
+  //   }
+  //   }
+  return quotesData[
+    Math.floor(Math.random() * 1643)
+  ];
+}
+function getQuote() {
+  let randomQuote = getRandomQuote();
+  currentQuote = randomQuote.text;
+  if (randomQuote.author != null){
     currentAuthor = randomQuote.author;
   }
-  $('#text').text(currentQuote);
-  document.getElementsByClassName("quote-text").innderhtml = currentQuote;
+  else{
+    currentAuthor = "unknown"
+  }
+  console.log(currentQuote)
+  console.log(currentAuthor)
+  $('.quote-text').text(currentQuote);
+  $('#author').text(currentAuthor);
+}
+
+$(document).ready(function () {
+  getQuotes().then(() => {
+    getQuote();
+  });
+  $('#new-quote').on('click', getQuote);
+});
