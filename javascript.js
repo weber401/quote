9dde6f601dfe41301cb441d77b759e7da94ce09e
Weber <52739961+weber401@users.noreply.github.com>
2024-02-16 16:39:47 +0545

    some corrections

--- javascript.js
+++ javascript.js
@@ -40,43 +40,34 @@ console.log(combo.first)
 
 // for api call
 function getQuotes(){
-// const settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "https://type.fit/api/quotes",
-//   "method": "GET"
-// }
+  console.log("test");
+  fetch('path/to/local/file.json')
+    .then(response => response.json())
+    .then(jsonQuotes => {
+      console.log(jsonQuotes); // This will log the fetched JSON data
+      // Your further processing logic here
+      return jsonQuotes
+    })
+    .catch(error => console.error('Error fetching JSON:', error));
 
-// $.ajax(settings).done(function (response) {
-//   const data = JSON.parse(response);
-//   quotesData = JSON.parse(response)
-//   console.log("Data:")
-//   console.log(quotesData);
-
-  return $.ajax({
-    headers: {
-      Accept: 'application/json'
-    },
-    url:
-      'https://type.fit/api/quotes',
-    success: function (jsonQuotes) {
-      if (typeof jsonQuotes === 'string') {
-        quotesData = JSON.parse(jsonQuotes);
-        console.log('quotesData');
-        console.log(quotesData);
-      }
-    } 
-});
+//THE CODE BELOW ONLY WORKS ON LOCAL MAHCINE I THINK
+//   return $.ajax({
+//     headers: {
+//       Accept: 'application/json'
+//     },
+//     url:
+//       'https://type.fit/api/quotes',
+//     success: function (jsonQuotes) {
+//       if (typeof jsonQuotes === 'string') {
+//         quotesData = JSON.parse(jsonQuotes);
+//         console.log('quotesData');
+//         console.log(quotesData);
+//       }
+//     } 
+// });
 }
  
 function getRandomQuote() {
-  // for (var number in quotesData){
-  //   if (number ==Math.floor(Math.random() * 100)){
-  //     console.log(number)
-  //     console.log(quotesData[number])
-  //     return quotesData[number]
-  //   }
-  //   }
   return quotesData[
     Math.floor(Math.random() * 1643)
   ];
